0d6b7ccd7a65921aa9f06e510116b1bc9c66aa08
Михаил <24mishka91@gmail.com>
2021-08-22 21:10:14 +0500

    init2

--- dist/js/promo.js
+++
@@ -1,2 +0,0 @@
-ready(function(){new Swiper(".promo__slider-inner",{wrapperClass:"promo__slider-content",slideClass:"promo__slide",slidesPerView:1,setWrapperSize:!0,loop:!1,pagination:{el:".promo__dots .dots",dynamicBullets:!1}})});
-//# sourceMappingURL=maps/promo.js.map
